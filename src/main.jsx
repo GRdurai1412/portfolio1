6f175717bcc10012c56a4cb07f55465daaa1b774
GRdurai1412 <158143482+GRdurai1412@users.noreply.github.com>
2025-05-28 10:10:40 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,10 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <Routes>
     <App />
+    </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
